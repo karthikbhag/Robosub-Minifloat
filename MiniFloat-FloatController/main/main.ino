a7f8e1cf70ae91559d80a8b0dc72ace0bc880bbd
Karthik Bhagavatula <kbhagavatula12@gmail.com>
2025-05-09 16:58:41 -0500

    Update main.ino

--- MiniFloat-FloatController/main/main.ino
+++ MiniFloat-FloatController/main/main.ino
@@ -6,6 +6,21 @@
 #include <OneWire.h> 
 #include <stdlib.h>
 
+const int pwmPin = 19;
+const int pwmChannel = 0;
+const int freq = 5000;
+const int resolution = 8;
+
+// void setup() {
+//   Serial.begin(115200);
+//    ledcSetup(pwmChannel, freq, resolution);
+//   ledcAttachPin(pwmPin, pwmChannel);
+//   ledcWrite(pwmChannel, 128);  // 50% power
+// }
+
+// void loop() {
+// }
+
 // Replace these with your actual network credentials
 const char* ssid = "ESP32-Minifloat-Point";
 const char* password = "RubberDuckies";
@@ -13,9 +28,9 @@ const char* password = "RubberDuckies";
 // Motor control pins
 const int MOTOR_DIR = 21;
 const int MOTOR_PWM = 19;
-const int pwmChannel = 0;
-const int pwmFreq = 5000;
-const int pwmResolution = 8;
+// Motor control pins
+const int MOTOR_DIR = 21;     // Direction control
+const int MOTOR_PWM = 19;     // Strength control via PWM
 
 WebServer server(80);
 
@@ -23,24 +38,21 @@ void setup() {
   Serial.begin(115200);
 
   pinMode(MOTOR_DIR, OUTPUT);
-  ledcSetup(pwmChannel, pwmFreq, pwmResolution);
-  ledcAttachPin(MOTOR_PWM, pwmChannel);
-  ledcWrite(pwmChannel, 0); // Start with motor off
-  digitalWrite(MOTOR_DIR, HIGH); 
-  ledcWrite(pwmChannel, 200);
+  pinMode(MOTOR_PWM, OUTPUT);
+
+  analogWrite(MOTOR_PWM, 0); // Start motor off
 
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi...");
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\nWiFi connected!");
-  Serial.println(WiFi.localIP());
+  Serial.println("\nWiFi connected. IP: " + WiFi.localIP().toString());
 
   server.on("/post", HTTP_POST, handlePostRequest);
   server.begin();
-  Serial.println("Server started.");
+  Serial.println("Server started");
 }
 
 void loop() {
@@ -63,19 +75,18 @@ void handlePostRequest() {
   }
 
   String action = doc["action"];
-  Serial.print("Received action: ");
-  Serial.println(action);
+  Serial.println("Received action: " + action);
 
   if (action == "inflate") {
     digitalWrite(MOTOR_DIR, HIGH);
-    ledcWrite(pwmChannel, 200);
+    analogWrite(MOTOR_PWM, 200); // Strength = ~80%
     server.send(200, "text/plain", "Inflating...");
   } else if (action == "deflate") {
     digitalWrite(MOTOR_DIR, LOW);
-    ledcWrite(pwmChannel, 200);
+    analogWrite(MOTOR_PWM, 200);
     server.send(200, "text/plain", "Deflating...");
   } else {
-    ledcWrite(pwmChannel, 0);
+    analogWrite(MOTOR_PWM, 0);  // Stop motor
     server.send(400, "text/plain", "Unknown action");
   }
 }
